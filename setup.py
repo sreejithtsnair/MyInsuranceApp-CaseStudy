90286d6d3f06906893f9ccd458944096c92e88a1
Rincy Issac <rincy@allianz.com>
2022-07-07 18:37:38 +0200

    first commit

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-package-example',
-    version='0.2',
+    name='myinsuranceapp',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
     long_description=open('README.txt').read(),
     install_requires=['numpy'],
-    url='https://github.com/BillMills/python-package-example',
-    author='Bill Mills',
-    author_email='myemail@example.com'
+    url='https://github.com/sreejithtsnair/MyInsuranceApp-CaseStudy.git',
+    author='UnaxRincySree',
+    author_email='team1@example.com'
 )
+
